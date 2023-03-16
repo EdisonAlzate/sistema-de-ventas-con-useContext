da54e395494df74528405d918322d2f3891eb00d
EdisonAlzate <edisonalzate11@gmail.com>
2023-03-16 12:24:41 -0500

    Se creo useContext

--- sistema-ventas-contex/src/App.js
+++ sistema-ventas-contex/src/App.js
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import { NotFound } from "./components/404/NotFound";
 import { Cart } from "./components/Cart/Cart";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
+import { ProviderUsuario } from "./contexts/global";
 
 function App() {
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [count, setCount] = useState(0);
+  const [addCar, setAddCart] = useState(0);
+  const [current, setCurrent] = useState('');
+
   return (
-    <div className="App">
-    <Routes>
-    <Route path="/" element={<Login />} />
-    <Route index path="home" element={<Home />} />
-    <Route path="cart" element={<Cart />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-    </div>
+    <ProviderUsuario
+      value={{
+        user,
+        count,
+        addCar,
+        current,
+        setCurrent,
+        setAddCart,
+        setCount,
+        setUser,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route index path="home" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ProviderUsuario>
   );
 }
 
